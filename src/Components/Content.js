707ae95a2d7429cb5303d05f522de687e4e4889d
andreasski <andrefjeld@gmail.com>
2018-09-07 15:34:27 +0200

    Removed references to files that doesn't exist

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import '../css/slideshow.css';
-import Category from './Category.js';
-import Media from './Media.js';
-import Nav from './Nav.js';
 
 export default class Content extends Component {
   render() {
     return (
         <div className="content">
-          <Category />
-          <Media />
-          <Nav />
+
         </div>
     );
   }
