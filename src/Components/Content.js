062a409047d9f6158f3bbc3a45858f5fb5e61a3d
andreasski <andrefjeld@gmail.com>
2018-09-07 15:13:21 +0200

    Altered some elements and applied some styling for mobile

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import '../css/slideshow.css';
+import Category from './Category.js';
+import Media from './Media.js';
+import Nav from './Nav.js';
 
-export default class Content extends Component{
+export default class Content extends Component {
   render() {
     return (
         <div className="content">
-          <p> CONTENTS </p>
+          <Category />
+          <Media />
+          <Nav />
         </div>
     );
   }
