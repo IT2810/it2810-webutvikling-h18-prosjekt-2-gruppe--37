e8f11cea9b54dd0c8a2bd43b4e884f5444cd86c0
mikkel21 <mbnygard@stud.ntnu.no>
2018-09-21 17:01:43 +0200

    #11 Changed Header to Gallery Changed title to Gallery Changed icon to custom icon

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -4,7 +4,7 @@ export default class Logo extends Component{
   render() {
     return (
         <div className="logo">
-          <h2>Kaktus fra Mars</h2>
+          <h2>Gallery</h2>
         </div>
     );
   }
